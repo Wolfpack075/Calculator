eab24feca23df7ad578806220dd9b7e827ccd92c
Kazi <kazifahimtahmid@gmail.com>
2023-03-12 09:23:01 +0600

    branch add korsi ar commit korsi

---
+++ src/main/java/com/houarizegai/calculator/hudai1.java
@@ -0,0 +1,38 @@
+package com.houarizegai.calculator;
+
+public class hudai1 {
+
+    private int a1;
+    private int a2;
+    private string zz;
+
+    public hudai1(int a1, int a2, string zz) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.zz = zz;
+    }
+
+    public int getA1() {
+        return a1;
+    }
+
+    public int getA2() {
+        return a2;
+    }
+
+    public string getZz() {
+        return zz;
+    }
+
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+
+    public void setA2(int a2) {
+        this.a2 = a2;
+    }
+
+    public void setZz(string zz) {
+        this.zz = zz;
+    }
+}
